49e23307a5810805ab58b10dba91096b8c5672fc
Kevin Stone <kevinastone@gmail.com>
2014-02-26 15:52:40 -0800

    Improved where clause reduction.
    Signed-off-by: Kevin Stone <kevinastone@gmail.com>

--- cursor_pagination/query.py
+++ cursor_pagination/query.py
@@ -21,12 +21,36 @@ REDUCTION_OPERATIONS = {
 }
 
 
+def _unroll_where_clause(where):
+    """
+    Attempts to unroll redundantly nested where clauses (like AND( AND( something = 1)))
+    """
+    # Look to see if we are just nested in single branch nodes
+    while getattr(where, 'children', None):
+        if len(where.children) != 1:
+            break
+        child = where.children[0]
+        if isinstance(child, (list, tuple)):
+            where = child
+            break
+        elif where.connector != child.connector:
+            break
+        where = child
+    return where
+
+
 def _reduce_redundant_clauses(where):
     clauses = defaultdict(list)
     other_clauses = []
     duplicate_found = False
 
+    where = _unroll_where_clause(where)
+
+    if not hasattr(where, 'children'):
+        return where
+
     for child in where.children:
+        child = _unroll_where_clause(child)
         if isinstance(child, (list, tuple)) and isinstance(child[0], Constraint):
             constraint, lookup_type, negation, value = child
             constraint_params = (constraint.alias, constraint.col, constraint.field)
@@ -59,8 +83,20 @@ def _reduce_redundant_clauses(where):
 
 
 def reduce_redundant_clauses(query):
+    """
+    Attempts to remove redundant where clauses that are superseded by more stringest clauses.
+
+    For Example: "select * from example_table where id > 1 and id > 2"
+    Can be reduced to: "select * from example_table where id > 2"
+    """
 
     query = query.clone()
-    query.where = _reduce_redundant_clauses(query.where)
+    where = _reduce_redundant_clauses(query.where)
+    if isinstance(where, (list, tuple)):
+        constraint, lookup_type, negation, value = where
+        where = query.where_class()
+        clause = (constraint, lookup_type, value)
+        where.add(clause, AND)
+    query.where = where
 
     return query
